03e5a96f37cbb024265f8b77dde86f6dc8914bdd
Paul Pineda <paul@pineda.codes>
2017-08-03 04:56:16 +0100

    All working in test jsut need to transfer to API

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@
 */
 import issues from './issues.js';
 
-function DesSurvey(definition){
+function DesSurveyReport(definition){
   var defaultDefinition = {
     issues: [],
     styles: {},
-    container: 'des-survey'
+    container: 'des-survey',
+    report_container: 'des-survey-report'
   };
 
   var def = Object.assign({}, defaultDefinition, definition);
@@ -31,6 +32,7 @@ function DesSurvey(definition){
           has_issue_answered: false,
           resolved: false,
           resolved_answered:false,
+          report_answered:false,
           id: survey_id + '_' + index
         }
       );
@@ -50,8 +52,8 @@ function DesSurvey(definition){
     return surveys[survey_id];
   }
 
-  function setIssueState( survey_id, issue_id, new_state_object ) {
-    var issues_array = getStoreSurveyByID(survey_id),
+  function setIssueState( survey_id, issue_id, new_state_object, renderFn) {
+    const issues_array = getStoreSurveyByID(survey_id),
       matched_issues = issues_array.filter( ( issue ) => issue.id === issue_id );
 
     if( matched_issues.length < 1)
@@ -65,6 +67,12 @@ function DesSurvey(definition){
     });
 
     storeSurveyByID( survey_id, new_issue_array_state );
+
+    if( renderFn ) {
+      renderFn( survey_id );
+    } else {
+      renderSurvey( survey_id );
+    }
   }
 
 
@@ -72,39 +80,60 @@ function DesSurvey(definition){
     document.body.addEventListener( 'click', function(e) {
       var issue_id = e.target.classList.value,
         target_value = e.target.value;
-      if ( e.target && e.target.matches('div.des-survey-has-issue > button') ) {
-        const actual_bool = $stringBoolToBool(target_value);
-        if (actual_bool )
-          setIssueState( survey_id, issue_id, { has_issue: actual_bool, has_issue_answered: true });
-        else
+      if ( e.target ){
+        if ( e.target.matches( 'div.des-survey-has-issue > button') ) {
+          const actual_bool = $stringBoolToBool( target_value );
+
+          if ( actual_bool ) {
+            setIssueState( survey_id, issue_id, { has_issue: actual_bool, has_issue_answered: true });
+          } else {
+            setIssueState( survey_id, issue_id, {
+              has_issue: actual_bool,
+              has_issue_answered: true,
+              resolved: true,
+              resolved_answered: true
+            });
+          }
+
+        }
+
+        if ( e.target.matches('div.des-survey-resolve-issue > button') ) {
           setIssueState( survey_id, issue_id, {
-            has_issue: actual_bool,
-            has_issue_answered: true,
-            resolved: true,
+            resolved: $stringBoolToBool(target_value),
             resolved_answered: true
           });
+        }
 
-        renderSurvey( survey_id );
+        if ( e.target.matches('div.des-survey-submit > button') ) {
+          console.log('setDataValue', getStoreSurveyByID( survey_id ));
+          //window.setDataValue( getStoreSurveyByID( survey_id ) )
+          createReport( survey_id );
+        }
+
+        if ( e.target.matches('span.des-report-resolve-issue > button') ) {
+          setIssueState( survey_id, issue_id, {
+            resolved: $stringBoolToBool(target_value),
+            report_answered: true
+          }, renderReport);
+        }
+
+        if ( e.target.matches('div.des-report-submit > button') ) {
+          console.log('submit survery to suspend data', getStoreSurveyByID( survey_id ));
+          //window.setDataValue( getStoreSurveyByID( survey_id ) );
+          const resolve_buttons_nodelist = document.querySelectorAll('span.des-report-resolve-issue > button');
+          resolve_buttons_nodelist.forEach(button => button.style.display = 'none');
+        }
+
+        e.target.style.display = 'none';
       }
-
-
-
-      if ( e.target && e.target.matches('div.des-survey-resolve-issue > button') ) {
-        setIssueState( survey_id, issue_id, {
-          resolved: $stringBoolToBool(target_value),
-          resolved_answered: true
-        });
-        renderSurvey( survey_id );
-      }
-
     });
   }
 
 
   function renderSurvey( survey_id ){
     var container = document.querySelector('.'+def.container),
-      dom_as_string = $objectArrayToDomString( getStoreSurveyByID ( survey_id ) );
-    container.innerHTML = createDivTextDomString(survey_id, dom_as_string);
+      dom_as_string = $surveyStateToDomString( getStoreSurveyByID ( survey_id ) );
+    container.innerHTML = createStringDomTag( 'div', survey_id, dom_as_string );
   }
 
   function createIssueDomString(issue){
@@ -112,40 +141,58 @@ function DesSurvey(definition){
 
     if ( issue.resolved_answered && issue.has_issue_answered ) {
       const resolved = ( issue.resolved ) ? 'resolved':'unresolved';
-      visibleElements += createDivTextDomString( 'des-survey-issue-text ' + resolved, issue.issue );
-      
-      return createDivTextDomString( 'des-survey-issue-' + issue.id, visibleElements);
+      visibleElements += createStringDomTag( 'div', 'des-survey-issue-text ' + resolved, issue.issue );
+
+      return createStringDomTag( 'div', 'des-survey-issue-' + issue.id, visibleElements);
     }
 
 
     if ( issue.has_issue_answered ) {
       visibleElements +=
-      createDivTextDomString( 'des-survey-suggested-res', issue.suggested_res ) +
-      createButtonDomString( 'des-survey-resolve-issue', issue.id, [
+      createStringDomTag( 'div', 'des-survey-suggested-res', issue.suggested_res ) +
+      createStringDomButtons( 'div', 'des-survey-resolve-issue', issue.id, [
         { label: 'Unresovled', value: false },
         { label: 'Resolved', value: true }
       ]);
 
     } else {
       visibleElements +=
-        createDivTextDomString( 'des-survey-issue-text', issue.issue ) +
-        createDivTextDomString( 'des-survey-question', issue.question ) +
-        createButtonDomString( 'des-survey-has-issue', issue.id, [
+        createStringDomTag( 'div', 'des-survey-issue-text', issue.issue ) +
+        createStringDomTag( 'div', 'des-survey-question', issue.question ) +
+        createStringDomButtons( 'div', 'des-survey-has-issue', issue.id, [
           { label: 'No', value: false },
           { label: 'Yes', value: true }
         ]);
     }
 
-    return createDivTextDomString( 'des-survey-issue-' + issue.id, visibleElements);
+    return createStringDomTag( 'div', 'des-survey-issue-' + issue.id, visibleElements);
   }
 
-  function createDivTextDomString( container_class, inner_html ){
-    return '<div class="' + container_class + '">' + inner_html + '</div>';
+  function createReportDomString ( issue ) {
+    const resolved = ( issue.resolved ) ? 'resolved':'unresolved';
+    const buttons = ( issue.report_answered || issue.resolved) ?
+      ''
+      :createStringDomButtons( 'span', 'des-report-resolve-issue', issue.id, [
+        { label: 'Resolved', value: true }
+      ]);
+
+    let visibleElements = '';
+    visibleElements += createStringDomTag(
+      'div',
+      'des-report-issue-text ' + resolved,
+      issue.issue + buttons
+    );
+
+    return createStringDomTag( 'div', 'des-report-issue-' + issue.id, visibleElements);
   }
 
-  function createButtonDomString( container_class, button_class, button_array ){
+  function createStringDomTag( tag, container_class, inner_html ){
+    return '<' + tag + ' class="' + container_class + '">' + inner_html + '</' + tag + '>';
+  }
+
+  function createStringDomButtons( tag, container_class, button_class, button_array ){
     return (
-      '<div class="' + container_class + '">' +
+      '<' + tag + ' class="' + container_class + '">' +
         button_array.reduce( ( acc, button ) => {
           acc += (
             '<button class="' + button_class + '" value="'+ button.value + '">' +
@@ -154,29 +201,79 @@ function DesSurvey(definition){
           );
           return acc;
         }, '') +
-      '</div>'
+      '</' + tag + '>'
     );
   }
 
+  function createReport( survey_id ) {
+
+
+    renderReport( survey_id );
+  }
+
+  function renderReport( survey_id ){
+    const submission = getStoreSurveyByID( survey_id );
+    //const unresolved = submission.filter( issue => !issue.resolved);
+    const container = document.querySelector('.'+def.report_container);
+    const dom_as_string = $reportToDomString( submission );
+
+    container.innerHTML = createStringDomTag( 'div', survey_id, dom_as_string );
+  }
 
   // HELPER METHODS PREFIXED WITH A '$' SIGN
   function $stringBoolToBool( value ) {
     return value === 'true';
   }
 
-  function $objectArrayToDomString(issues_array){
-    var issues_as_dom_string = issues_array.reduce(function(acc,issue){
+  function $surveyStateToDomString( issues_array ) {
+    const issues_as_dom_string = issues_array.reduce( (acc,issue) => {
       return acc += createIssueDomString(issue);
     }, '');
 
-    return issues_as_dom_string;
+    let submit = '';
+
+    if (allAnswered( issues_array )) {
+      submit =  createStringDomButtons(
+        'div',
+        'des-survey-submit',
+        null,
+        [{label: 'Submit', value: ''}]
+      );
+    }
+
+    return issues_as_dom_string + submit;
   }
 
+  function $reportToDomString( report_array ) {
+    const report_as_dom_string = report_array.reduce( (acc, issue) => {
+      return acc += createReportDomString(issue);
+    }, '');
+
+    // show submit on the report page unliess it has been clicked
+    const submit = createStringDomButtons(
+      'div',
+      'des-report-submit',
+      null,
+      [{label: 'Submit', value: ''}]
+    );
+
+    return report_as_dom_string + submit;
+  }
+
+  function allAnswered(array){
+    return array.filter( issue => !(issue.has_issue_answered && issue.resolved_answered) ).length < 1;
+  }
+
+  // function allUnresolvedAnswered(array){
+  //   return array.filter( issue => !(issue.report_answered && issue) ).length < 1;
+  // }
+
   var public_api = {
-    createSurvey
+    createSurvey,
+    createReport
   };
 
   return public_api;
 }
 
-window.DesSurvey = DesSurvey({issues}).createSurvey('survey-page-0');
+window.DesSurveyReport = DesSurveyReport({issues}).createSurvey('survey-page-0');
---
+++ src/submitted_issues.js
@@ -0,0 +1,41 @@
+export default [{
+  "location": "DefaultLoc",
+  "issue": "Issue Title 111",
+  "question": "Lorem ipsum question 111",
+  "suggested_res": "Suggested resolution text 111",
+  "has_issue": false,
+  "has_issue_answered": true,
+  "resolved": true,
+  "resolved_answered": true,
+  "id": "survey-page-0_0"
+}, {
+  "location": "DefaultLoc",
+  "issue": "Issue Title 222",
+  "question": "Lorem ipsum question 222",
+  "suggested_res": "Suggested resolution text 222",
+  "has_issue": true,
+  "has_issue_answered": true,
+  "resolved": false,
+  "resolved_answered": true,
+  "id": "survey-page-0_1"
+}, {
+  "location": "DefaultLoc",
+  "issue": "Issue Title 333",
+  "question": "Lorem ipsum question 333",
+  "suggested_res": "Suggested resolution text 333",
+  "has_issue": true,
+  "has_issue_answered": true,
+  "resolved": true,
+  "resolved_answered": true,
+  "id": "survey-page-0_2"
+}, {
+  "location": "DefaultLoc",
+  "issue": "Issue Title 444",
+  "question": "Lorem ipsum question 444",
+  "suggested_res": "Suggested resolution text 444",
+  "has_issue": true,
+  "has_issue_answered": true,
+  "resolved": true,
+  "resolved_answered": true,
+  "id": "survey-page-0_3"
+}]
